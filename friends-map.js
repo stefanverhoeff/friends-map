c33af01a39e333e4e8d2cb52cc5b47d956ff273d
Stefan Verhoeff <stefan_verhoeff@yahoo.com>
2013-01-30 00:23:48 +0100

    Refactoring, extracted callback mess into functions

--- friends-map.js
+++ friends-map.js
@@ -2,12 +2,13 @@
     "use strict";
 
     var limit = 1000;
+    var friends = [];
 
     if (development) {
-//        limit = 25;
+        limit = 25;
     }
 
-    var loadPictures = function () {
+    var loadFriends = function (callback) {
         var fbRequestFriends =
             '/me/friends'
             + '?fields=name,username,picture,location,hometown'
@@ -21,50 +22,71 @@
                 return;
             }
 
-            console.log("You have " + response.data.length + " friends");
-            response.data.forEach(function (friend) {
-                var location = friend.location || friend.hometown;
-
-                if (location && location.name) {
-                    var profilePic = $("<img>")
-                        .attr('src', friend.picture.data.url)
-                        .attr('title', friend.name + ' - ' + location.name)
-                        .get();
-
-                    // Show list of friends below map
-                    $('#friends').append(profilePic);
-
-                    // Lookup location lat/lng
-                    FB.api(location.id, function (response) {
-                        if (response.error) {
-                            console.log('Failed to lookup location', response.error);
-                            return;
-                        }
-
-                        var position = [response.location.latitude, response.location.longitude];
-
-                        $('#mapContainer').jHERE('marker',
-                            position,
-                            {
-                                icon: friend.picture.data.url,
-                                anchor: {x: 12, y: 32},
-                                click: function() {
-                                    $('#mapContainer').jHERE('bubble',
-                                        position, {
-                                                   content: '<a href="https://www.facebook.com/' + friend.username + '" target="_blank">'+friend.name+'</a>' + ' says Hallo from ' + response.name,
-                                                   closable: true,
-                                                   onclose: function(){}
-                                                  })
-                                    }
-                            });
-                        });
-                }
-            });
+            friends = response.data;
+            callback && callback();
         });
     };
 
+    var loadFriendsLocation = function () {
+        friends.forEach(function (friend) {
+            var location = friend.location || friend.hometown;
+
+            if (location && location.name) {
+                lookupFriendLocation(friend, location);
+            }
+        });
+    };
+
+    var lookupFriendLocation = function (friend, location) {
+        // Lookup location lat/lng
+        FB.api(location.id, function (response) {
+            if (response.error) {
+                console.log('Failed to lookup location', response.error);
+                return;
+            }
+
+            friend.location = response;
+            friend.location.position = [response.location.latitude, response.location.longitude];
+
+            showFriendOnMap(friend);
+        });
+    };
+
+    var showFriendOnMap = function (friend) {
+        friend.profilePic = $("<img>")
+            .attr('src', friend.picture.data.url)
+            .attr('title', friend.name + ' - ' + location.name)
+            .get();
+
+        // Show list of friends on right hand side
+        $('#friends').append(friend.profilePic);
+
+        $('#mapContainer').jHERE('marker',
+            friend.location.position,
+            {
+                icon: friend.picture.data.url,
+                anchor: {x: 12, y: 32},
+                click: function() {
+                    showFriendBubble(friend);
+                }
+            });
+    };
+
+    var showFriendBubble = function (friend) {
+        $('#mapContainer').jHERE('bubble',
+            friend.location.position,
+            {
+                content: '<a href="https://www.facebook.com/' + friend.username + '" target="_blank">'+friend.name+'</a>' + ' says Hallo from ' + friend.location.name,
+                closable: true,
+                onclose: function() {}
+            });
+    };
+
     var loadPicturesDefaults = function () {
-        loadPictures();
+        loadFriends(function () {
+            console.log("You have " + friends.length + " friends");
+            loadFriendsLocation();
+        });
 
         setTimeout(function () {
             console.log('Got ' + $('#friends img').length + ' friends on the map');
@@ -74,7 +96,7 @@
     var loadMap = function () {
         $('#mapContainer').jHERE({
             enable: ['behavior', 'zoombar', 'scalebar', 'typeselector', 'positioning'],
-            center: [0, 0],
+            center: [35, 0],
             zoom: 2
         });
     };
